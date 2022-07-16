4b7e63338d4485796337a624db2fb79b6770c421
Rodrigo Arrechea <rodrigoarrechea.dg@gmail.com>
2022-07-15 22:56:24 -0300

    pruebas

---
+++ Clase 3/app.js
@@ -0,0 +1,9 @@
+// for(let contador = 1; contador <= 5; contador++){
+// //codigo a ejecutar
+// console.log ("Hola mundo")
+// }
+
+for(let i = 0; i < 10; i += 2){
+    //codigo a ejecutar forma pro
+    console.log (i)
+    }
